f4e6521db96a13420c3625db2f6148e5de8f7783
Ayanas <ncsan93@gmail.com>
2024-04-02 13:34:30 +0530

    Delete manage.py

--- manage.py
+++
@@ -1,9 +0,0 @@
-from app.application import flask_app
-from app.routes import routes_bp
-from app.database import create_database
-flask_app.register_blueprint(routes_bp)
-
-if __name__ == "__main__":
-    create_database()
-    flask_app.run(debug=True)  # Set debug=True for development purposes
-
