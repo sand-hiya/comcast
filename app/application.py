6cd60546674b0c99ad4a6ed1a3a92407571f7d5c
Ayanas <ncsan93@gmail.com>
2024-04-02 13:32:55 +0530

    Delete app directory

--- app/application.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-flask_app = Flask(__name__)
-flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../covid_data.db'
-flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy()
-db.init_app(flask_app)
